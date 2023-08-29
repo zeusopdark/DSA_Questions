c0def034a353b12ca1050a53acdeabca10eaafd5
zeusopdark <ankitnishad400@gmail.com>
2023-08-29 18:26:56 +0530

    Added the solution of problem

---
+++ Content/LinkedList/AddTwoNumbers.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(0);
+        ListNode prev=dummy;
+        
+        int count=0;
+        while(l1!=null||l2!=null||count>0){
+            int sum=0;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
+            
+            sum+=count;
+            
+            ListNode node=new ListNode(sum%10);
+            count=sum/10;
+            prev.next=node;
+            prev=node;
+        }
+        return dummy.next;
+    }
+}
